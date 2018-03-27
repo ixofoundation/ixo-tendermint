de4e7b04cebddb5e6f3c93b3ed5c47bce3c6ab1b
Nicolaas Vercuiel <nicolaas.vercuiel73@gmail.com>
2018-03-27 11:22:35 +0200

    # Got working blockchain chat app. # Added all files to run out of the box.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ixo-tendermint
+
+Send message:
+curl http://localhost:3000/txs -d '{"sender": "Nic", "message": "Hello"}'
---
+++ ixo-node1.js
@@ -0,0 +1,25 @@
+let lotion = require('lotion');
+
+let app = lotion({
+    genesis: 'genesis.json',
+    tendermintPort: 46657,
+    initialState: { messages: [] },
+    p2pPort: 46660,
+    logTendermint: true,
+    keys: 'privkey0.json',
+    peers: ['localhost:46661']
+});
+
+app.use((state, tx, chainInfo) => {
+    if (typeof tx.sender === 'string' && typeof tx.message === 'string') {
+        state.messages.push({ sender: tx.sender, message: tx.message })
+    }
+});
+
+app.useBlock(function (state, chainInfo) {
+    // do something once per block here
+});
+
+app.listen(3000).then(({ GCI }) => {
+    console.log(GCI)
+});
